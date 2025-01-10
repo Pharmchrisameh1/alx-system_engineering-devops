4aecb6fc43130d25c54ee63060e53e51945f60f2
Rx Chris Ameh <chris1jnr@gmail.com>
2025-01-10 09:44:50 -0800

    0-create_a_file.pp

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# This manifest creates a school file in the /tmp dir
+file { 'createschool':
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
